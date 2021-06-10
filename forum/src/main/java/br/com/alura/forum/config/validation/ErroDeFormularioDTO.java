767b96bc2d403e0c61236eff53f3e5ef233f8350
Alexandre Marinho de Souza Júnior <alexandre.masj@gmail.com>
2021-06-10 20:14:27 -0300

    Criado um intercepter para melhorar o retorno de erro do Bean Validation

---
+++ forum/src/main/java/br/com/alura/forum/config/validation/ErroDeFormularioDTO.java
@@ -0,0 +1,27 @@
+package br.com.alura.forum.config.validation;
+
+public class ErroDeFormularioDTO {
+	
+	private String campo;
+	private String erro;
+	
+	
+	public ErroDeFormularioDTO(String campo, String erro) {
+		super();
+		this.campo = campo;
+		this.erro = erro;
+	}
+
+
+	public String getCampo() {
+		return campo;
+	}
+
+
+	public String getErro() {
+		return erro;
+	}
+	
+	
+	
+}
