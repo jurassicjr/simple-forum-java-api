45bc3f516e724b3f66c05927a0437eb46a77e1a4
Alexandre Marinho de Souza Júnior <alexandre.masj@gmail.com>
2021-06-10 21:21:24 -0300

    Alteração no TopicosController para tratar exceções de ID inválidos para tópicos.

--- forum/src/main/java/br/com/alura/forum/controllers/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controllers/TopicosController.java
@@ -2,12 +2,14 @@ package br.com.alura.forum.controllers;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,47 +30,70 @@ import br.com.alura.forum.repositories.TopicoRepository;
 @RestController
 @RequestMapping("/topicos")
 public class TopicosController {
-	
+
 	@Autowired
 	private TopicoRepository topicoRepository;
-	
+
 	@Autowired
 	private CursoRepository cursoRepository;
-	
+
 	@GetMapping
 	public List<TopicoDTO> lista(String nomeCurso) {
 		if (nomeCurso == null) {
-			List<Topico> topicos = topicoRepository.findAll();		
-			return TopicoDTO.converter(topicos);	
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDTO.converter(topicos);
 		}
-		List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);		
-		return TopicoDTO.converter(topicos);	
-		
+		List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+		return TopicoDTO.converter(topicos);
+
 	}
-	
+
+	@Transactional
 	@PostMapping
 	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
-		
+
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
-		
+
 		return ResponseEntity.created(uri).body(new TopicoDTO(topico));
 	}
-	
+
 	@GetMapping("/{id}")
-	public DetalhesDoTopicoDTO detalhar(@PathVariable Long id) {
-		Topico topico = topicoRepository.getById(id);
-		return new DetalhesDoTopicoDTO(topico);
+	public ResponseEntity<DetalhesDoTopicoDTO> detalhar(@PathVariable Long id) {
+		Optional<Topico> topico = topicoRepository.findById(id);
+		if (topico.isPresent()) {
+			return ResponseEntity.ok(new DetalhesDoTopicoDTO(topico.get()));
+		}
+		return ResponseEntity.notFound().build();
 	}
-	
-	
+
 	@PutMapping("/{id}")
 	@Transactional
-	public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
-		Topico topico = form.atualizar(id, topicoRepository);
-		
-		
-		return ResponseEntity.ok(new TopicoDTO(topico));
+	public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+		Optional<Topico> topico = topicoRepository.findById(id);
+
+		if (topico.isPresent()) {
+
+			Topico updatedTopico = form.atualizar(id, topicoRepository);
+			return ResponseEntity.ok(new TopicoDTO(updatedTopico));
+
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+
+		Optional<Topico> topico = topicoRepository.findById(id);
+		if (topico.isPresent()) {
+			topicoRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+
+		return ResponseEntity.notFound().build();
+
 	}
 }
